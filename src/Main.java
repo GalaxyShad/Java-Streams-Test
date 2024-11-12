c22e1f476784c8298d3a727829f4d577685e6d27
GalaxyShad <vadim.kadyrin@mail.ru>
2024-11-12 23:52:53 +0300

    [+] stream sequential and parallel versions

--- src/Main.java
+++ src/Main.java
@@ -20,10 +20,59 @@ public class Main {
 
         var countList = new int[]{5_000, 50_000, 250_000};
 
+//        lab1Measure(gen, countList);
+        lab2Measure(gen, countList);
+    }
+
+    public static void lab2Measure(LedStripGenerator gen, int[] countList) {
+        var millisDelay = 2;
+        var millisDelayString = millisDelay + "millis delay";
+
         for (int i = 0; i < 3; i++) {
-            appendToFile(countList[i] + ".csv",  "№ итерации;цикл;стрим;коллектор");
+            appendToFile(countList[i] + ".csv",
+                    "№ итерации;sequential;parallel" +
+                    ";sequentual " + millisDelayString +
+                    ";parallel " + millisDelayString);
         }
 
+        for (int i = 0; i < 15; i++) {
+            for (int count : countList) {
+                var ledStrips = gen.generate(count);
+
+                System.out.println("\nLedStrip Count: " + count);
+
+                var seq = measureStream(ledStrips, false, 0);
+                var par = measureStream(ledStrips, true, 0);
+
+                var seqDelayed = measureStream(ledStrips, false, millisDelay);
+                var parDelayed = measureStream(ledStrips, true, millisDelay);
+
+                appendToFile(count + ".csv", (i + 1) + ";" + seq + ";" + par + ";" + seqDelayed + ";" + parDelayed);
+            }
+        }
+
+        System.out.println("Приступаем к поиску количества элементов где параллельный и последовательный способы были бы одинаковыми по времени");
+        lab2findWhenSeqAndParTimeDurationEquals(gen);
+    }
+
+    public static void lab2findWhenSeqAndParTimeDurationEquals(LedStripGenerator gen) {
+        for (int i = 1; i < 50_000; i++) {
+            var ledStrips = gen.generate(i);
+
+            var seq = measureStream(ledStrips, false, 0);
+            var par = measureStream(ledStrips, true, 0);
+
+            if (seq != par)
+                continue;
+
+            System.out.println("\nКол-во элементов при котором параллельная реализаци : " + i);
+        }
+    }
+
+    public static void lab1Measure(LedStripGenerator gen, int[] countList) {
+        for (int i = 0; i < 3; i++) {
+            appendToFile(countList[i] + ".csv", "№ итерации;цикл;стрим;коллектор");
+        }
 
         for (int i = 0; i < 50; i++) {
             for (int count : countList) {
@@ -32,10 +81,10 @@ public class Main {
                 System.out.println("\nLedStrip Count: " + count);
 
                 var loop = measureLoop(ledStrips);
-                var stream = measureStream(ledStrips);
+                var stream = measureStreamSequential(ledStrips);
                 var collector = measureOwnCollector(ledStrips);
 
-                appendToFile(count + ".csv", (i+1) + ";" + loop + ";" + stream + ";" + collector);
+                appendToFile(count + ".csv", (i + 1) + ";" + loop + ";" + stream + ";" + collector);
             }
         }
     }
@@ -58,11 +107,21 @@ public class Main {
         return time;
     }
 
-    private static long measureStream(LedStrip[] stripList) {
+    private static long measureStreamSequential(LedStrip[] stripList) {
+        return measureStream(stripList, false, 0);
+    }
+
+    private static long measureStream(LedStrip[] stripList, boolean useParallel, int delayMillis) {
         var start = System.nanoTime();
-        var max = Arrays
-                .stream(stripList)
-                .max(Comparator.comparingDouble(LedStrip::averageColorTemperature));
+
+        var stream = Arrays.stream(stripList);
+
+        if (useParallel) {
+            stream = stream.parallel();
+        }
+
+        var max = stream.max(Comparator.comparingDouble((x) -> Delayer.delayAndExecute(delayMillis, x::averageColorTemperature)));
+
         var time = System.nanoTime() - start;
 
         System.out.println("Stream API (ns): " + time);
