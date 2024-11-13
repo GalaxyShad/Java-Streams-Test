e01fb1516633996b573e2f16bd520d0c78fe0294
GalaxyShad <vadim.kadyrin@mail.ru>
2024-11-13 16:48:20 +0300

    [~] refactor

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -14,7 +9,6 @@ public class Main {
         try (var writer = new BufferedWriter(new FileWriter(fileName, true))) {
             writer.write(content);
             writer.newLine(); // Добавляем новую строку после записи
-            System.out.println("Data successfully appended to the file.");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -24,11 +18,17 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         var gen = new LedStripGenerator();
-
         var countList = new int[]{5_000, 50_000, 250_000};
 
+        lab1(gen, countList);
+    }
+
+    private static void lab1(LedStripGenerator gen, int[] countList) {
         for (int i = 0; i < 3; i++) {
-            appendToFile(countList[i] + ".csv",  "№ итерации;цикл;стрим;коллектор");
+            var labelsString = "№ итерации;цикл;стрим;коллектор";
+
+            appendToFile(countList[i] + ".csv",  labelsString);
+            System.out.println(labelsString);
         }
 
 
@@ -36,58 +36,15 @@ public class Main {
             for (int count : countList) {
                 var ledStrips = gen.generate(count);
 
-                System.out.println("\nLedStrip Count: " + count);
+                var loop = Measurament.measureIterative(ledStrips);
+                var stream = Measurament.measureStream(ledStrips);
+                var collector = Measurament.measureOwnCollector(ledStrips);
 
-                var loop = measureIterative(ledStrips);
-                var stream = measureStream(ledStrips);
-                var collector = measureOwnCollector(ledStrips);
+                var dataString = (i+1) + ";\t\t\t" + loop + ";\t\t" + stream + ";\t\t" + collector;
 
-                appendToFile(count + ".csv", (i+1) + ";" + loop + ";" + stream + ";" + collector);
+                System.out.println("LedStrip Count: " + count + " data: " + dataString);
+                appendToFile(count + ".csv", dataString);
             }
         }
-    }
-
-    private static long measureIterative(LedStrip[] stripList) {
-        var start = System.nanoTime();
-
-        Map<Integer, ArrayList<LedStrip>> groupedByDistanceBetweenLeds = new HashMap<>();
-
-        for (LedStrip strip : stripList) {
-            int distance = strip.getDistanceBetweenLed();
-            groupedByDistanceBetweenLeds.computeIfAbsent(distance, _ -> new ArrayList<>()).add(strip);
-        }
-
-        var time = System.nanoTime() - start;
-
-        System.out.println("Iterative (ns): " + time);
-
-        return time;
-    }
-
-    private static long measureStream(LedStrip[] stripList) {
-        var start = System.nanoTime();
-
-        var groupedByDistanceBetweenLeds = Arrays
-                .stream(stripList)
-                .collect(Collectors.groupingBy(LedStrip::getDistanceBetweenLed));
-
-        var time = System.nanoTime() - start;
-
-        System.out.println("Stream API (ns): " + time);
-
-        return time;
-    }
-
-    private static long measureOwnCollector(LedStrip[] stripList) {
-        var start = System.nanoTime();
-
-        var groupedByDistanceBetweenLeds = Arrays.stream(stripList)
-                .collect(new LedStripCollector());
-
-        var time = System.nanoTime() - start;
-
-        System.out.println("Custom Aggregator (ns): " + time);
-
-        return time;
     }
 }
